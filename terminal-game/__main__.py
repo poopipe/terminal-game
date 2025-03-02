d9924679df4858e977e301bf129f26f215674718
poopipe <ill_ness@hotmail.com>
2025-03-02 13:02:56 +0000

    renaming package

---
+++ terminal-game/__main__.py
@@ -0,0 +1,10 @@
+'''https://www.nerdfonts.com/cheat-sheet'''
+from terminal_game.lib.engine import Game
+from terminal_game.lib.types import VecT
+from terminal_game.lib import entities
+
+if __name__ == '__main__':
+    game = Game()
+    game.init()
+
+
---
+++ terminal-game/lib/entities.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from .types import Entity, VecT
+
+
+class LandMine(Entity):
+    def __init__(self, name, game):
+        super().__init__(name, 'x', game)
+
+    def behave(self):
+        if self.position.x <= 0:
+            self.die()
+        # we dont want to move every frame
+        if self.game.frame_counter % 10 == 0:
+            self.move(VecT(-1,0))
+    
+    def die(self):
+        self.game.score += 10
+        self.game.entities.remove(self)
+        
+
+class Player(Entity):
+    def __init__(self, name, game):
+        super().__init__(name, '\uee25', game)
+
+    def collide(self, entity):
+        print(f'player collided with{entity}')
+        if isinstance(entity, LandMine):
+           self.die()
+
+    def die(self):
+        #self.game.entities.remove(self)
+        #self.glyph = 'Q'
+        self.game.mode = 2  # death screen
+        self.game.reset()
+
+
+
+
+
